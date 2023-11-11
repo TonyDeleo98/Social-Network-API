797cf983a1102dc9cb03014c44a534e68b542cac
TonyDeleo98 <Tonyd9819@gmail.com>
2023-11-11 10:31:46 -0500

    updated fix

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 // Import user and thought routes
-const userRoutes = require('./user-routes.js');
-const thoughtRoutes = require('./thought-routes.js');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 // Define endpoints for user and thought routes
 router.use('/user',userRoutes);
 router.use('/thought',thoughtRoutes);
